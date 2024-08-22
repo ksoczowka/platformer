da620b1091cbd90cf8ab347b5bdfb611271560f8
ksoczowka <ksoczowka.ks@gmail.com>
2024-08-22 19:17:20 +0200

    Added simple action using functionality and imporved movement with new gravity

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,30 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -600.0
+const SPEED = 600.0
+const JUMP_VELOCITY = -1200.0
 
+var is_action_used: bool = false
+
+@onready var ray_cast: RayCast2D = $RayCast2D
+
+func ray_cast_colliding():
+	velocity.y = JUMP_VELOCITY - 500
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if Input.is_action_pressed("action") and ray_cast.is_colliding() and not is_on_floor() and not is_action_used:
+		is_action_used = true
+		$AnimationPlayer.play("action_used")
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_released("action"):
+		is_action_used = false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
